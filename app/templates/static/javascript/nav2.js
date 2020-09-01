40c114f530e7ee2f58ae110fb4a2b6849a6c9031
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-01 12:08:39 +0200

    added Procfile

--- app/templates/static/javascript/nav2.js
+++ app/templates/static/javascript/nav2.js
@@ -5,14 +5,14 @@ const logo = document.querySelector('#theo');
 
 
 window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
+        if (window.scrollY > 20) {
             nav.classList.add('scroll');
             title.classList.add('disparition');
             head.classList.add('disparition')
             logo.classList.add('color');
         }
 
-        else if (window.scrollY < 300) {
+        else if (window.scrollY < 250) {
             nav.classList.remove('scroll');
             title.classList.remove('disparition');
             head.classList.remove('disparition');
